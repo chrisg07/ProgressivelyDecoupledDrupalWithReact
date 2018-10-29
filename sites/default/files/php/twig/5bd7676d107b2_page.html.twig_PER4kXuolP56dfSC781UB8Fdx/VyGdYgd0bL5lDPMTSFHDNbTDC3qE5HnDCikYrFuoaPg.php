f4e788f279fafd5b8234c2a823721d80a1deb2af
Christopher Guilliams <chris.guilliams@modea.com>
2018-10-29 16:42:06 -0400

    Tutorial #8: Using React to list content from Drupal

---
+++ sites/default/files/php/twig/5bd7676d107b2_page.html.twig_PER4kXuolP56dfSC781UB8Fdx/VyGdYgd0bL5lDPMTSFHDNbTDC3qE5HnDCikYrFuoaPg.php
@@ -0,0 +1,132 @@
+<?php
+
+/* themes/react_simple/templates/page.html.twig */
+class __TwigTemplate_9826bd013f6ac4fd04ed068cd223d0f39d93f9b998f28e88a28f21e30b73d67b extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("if" => 67);
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array('if'),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 45
+        echo "<div class=\"layout-container\">
+  ";
+        // line 60
+        echo "  <main role=\"main\">
+    <a id=\"main-content\" tabindex=\"-1\"></a>";
+        // line 62
+        echo "
+    <div class=\"layout-content\">
+      ";
+        // line 64
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "content", array()), "html", null, true));
+        echo "
+    </div>";
+        // line 66
+        echo "
+    ";
+        // line 67
+        if ($this->getAttribute(($context["page"] ?? null), "sidebar_first", array())) {
+            // line 68
+            echo "      <aside class=\"layout-sidebar-first\" role=\"complementary\">
+        ";
+            // line 69
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "sidebar_first", array()), "html", null, true));
+            echo "
+      </aside>
+    ";
+        }
+        // line 72
+        echo "
+    ";
+        // line 73
+        if ($this->getAttribute(($context["page"] ?? null), "sidebar_second", array())) {
+            // line 74
+            echo "      <aside class=\"layout-sidebar-second\" role=\"complementary\">
+        ";
+            // line 75
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "sidebar_second", array()), "html", null, true));
+            echo "
+      </aside>
+    ";
+        }
+        // line 78
+        echo "
+  </main>
+
+  ";
+        // line 81
+        if ($this->getAttribute(($context["page"] ?? null), "footer", array())) {
+            // line 82
+            echo "    <footer role=\"contentinfo\">
+      ";
+            // line 83
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "footer", array()), "html", null, true));
+            echo "
+    </footer>
+  ";
+        }
+        // line 86
+        echo "
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/react_simple/templates/page.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  101 => 86,  95 => 83,  92 => 82,  90 => 81,  85 => 78,  79 => 75,  76 => 74,  74 => 73,  71 => 72,  65 => 69,  62 => 68,  60 => 67,  57 => 66,  53 => 64,  49 => 62,  46 => 60,  43 => 45,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "themes/react_simple/templates/page.html.twig", "/Users/chris.guilliams/Projects/drupalLearning/themes/react_simple/templates/page.html.twig");
+    }
+}
--- themes/react_simple/js/index.js
+++ themes/react_simple/js/index.js
@@ -46,6 +46,7 @@ class ShowNodeContent extends React.Component {
           alignSelf: 'center',
         },
         content: {
+          height: 'auto',
           margin: '30px',
           padding: '30px',
           backgroundColor: '#eee',
@@ -107,12 +108,113 @@ class NodeContent extends React.Component {
       
 }
 
+class NodeItem extends React.Component {
+    render() {
+        return <NodeContent title = {this.props.attributes.title} content = {this.props.attributes.body.value}/>;
+    }
+}  
+  
+class NoData extends React.Component {
+    render() {
+        return <div>No articles found.</div>;
+    }
+}
+  
+class NodeList extends React.Component {
+    constructor() {
+        super();
+        this.state = {data: null};
+        this.loadNodeData = this.loadNodeData.bind(this);
+        this.updateData = this.updateData.bind(this);
+        this.validateData = this.validateData.bind(this);
+    }
+    
+    componentWillMount() {
+        this.loadNodeData();
+    }
+
+    loadNodeData() {
+    // This should point to your local Drupal instance.
+        const API_ROOT = 'http://localhost:8888/jsonapi/';
+        const url = `${API_ROOT}node/article`;
+        
+        fetch(url)
+            .then(function(response) {
+                return response.json();
+            })
+            .then((data) => this.updateData(data))
+            .catch(err => console.log('API got an error', err))
+    }
+      
+    updateData(responseData) {
+        this.validateData(responseData) ? this.setState( { data: responseData.data }, () => console.log(this.state)) 
+            : console.log('The data could not be updated because it contained an error.');
+    }
+
+    validateData(data) {
+        if (data === null) {
+            return false;
+        }
+        if (data.data === undefined ||
+            data.data === null ||
+            data.data.length === 0 ) {
+            return false;
+        }
+        return true;
+    }
+      
+
+    render() {
+        const style = {
+            nodeItem: {
+              border: '2px solid black',
+              margin: '10px',
+              padding: '10px',
+              backgroundColor: '#ccc',
+            },
+            nodeItemActive: {
+              border: '2px solid black',
+              margin: '10px',
+              padding: '10px',
+              backgroundColor: '#fff',
+            }
+        };
+
+        return (
+        <div>
+            <h2>Site content</h2>
+            {this.state.data !== null ?
+            this.state.data.map(item => {
+                let className = 'nodeItem';
+                let current = false;
+                if (Number(this.props.nid) === Number(item.attributes.nid)) {
+                    className = 'nodeItemActive';
+                    current = true;
+                }
+                console.log(className);
+                return (<div style={style[className]}> <NodeItem current={current} {...item} /> </div>);
+            } )
+            :
+            <NoData />
+            }
+        </div>
+        );
+    }
+}
+
 // # Example 1: Simple Hello World code
 ReactDOM.render(
     <NodeContent 
         title = {document.getElementById('react-content').getAttribute('data-title')}
         content = {document.getElementById('react-content').innerHTML}
     />,
-    document.getElementById('react-app')
-  );
+    document.getElementById('react-app-exercise5')
+);
+
+ReactDOM.render(
+    <NodeList
+        nid={document.getElementById('react-app-exercise8').getAttribute('data-nid')}
+    />,
+    document.getElementById('react-app-exercise8-container')
+);
   
